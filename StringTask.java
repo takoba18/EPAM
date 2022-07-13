6aac254df158081d0b5275f72f4d485e77cc88e0
takoba18 <62853794+takoba18@users.noreply.github.com>
2022-07-13 18:08:53 +0400

    Update StringTask.java

--- StringTask.java
+++ StringTask.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 import static java.lang.Math.max;
 
 public class StringTask {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        int ans = solve(s);
-        System.out.println(ans);
+        String input = in.nextLine();
+        int maxNumOfWords = new StringTask().solve(input);
+        System.out.println(maxNumOfWords);
     }
 
-    public static int solve(String s) {
-        int ans = 0;
-        String[] sentences = s.split("[.\\!\\?]");
-        for (String s1 : sentences) {
-            String[] words = s1.split(" ");
-            ans = max(ans, words.length);
+    private int solve(String input) {
+        int maxWords = 0;
+        String[] sentences = input.split("[.\\!\\?]");
+        for (String sentence : sentences) {
+            String[] words = sentence.split(" ");
+            maxWords = max(maxWords, words.length);
         }
-        return ans;
+        return maxWords;
     }
 }
